4db5f7cf94cbc9932c780be31925d1452aafcd0d
anubhav9454 <104710170+anubhav9454@users.noreply.github.com>
2023-02-13 23:24:24 +0530

    init_2

---
+++ pages/aboutUs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt-link to="/"> Index</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <h1 class="text-sm">Is it Working</h1>
+  <div class="bg-slate-200 p-10">
+    <nuxt-link to="/aboutUs">About Us</nuxt-link>
+    <div class=" flex items-center justify-between">
+      <div class="flex px-2 items-center justify-around">
+        <div class="bg-rose-500 font-semibold text-5xl text-white font-serif px-2 rounded-lg">TMBK</div>
+        <div class="font-bold">TMBK@Business</div>
+      </div>
+      <div class="flex gap-8">
+        <div class="">
+          Welcome
+        </div>
+        <div class="">
+          Dropdown
+        </div>
+        <div class="">
+          Left Sidebar
+        </div>
+        <div class="">
+          Right Sidebar
+        </div>
+        <div class="">
+          No Sidebar
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>

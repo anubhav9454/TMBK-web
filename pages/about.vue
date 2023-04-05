067dcefae59e8c756e34fa7b018035d071fd90ec
Manupal Choudhary <tech.manujpr@gmail.com>
2023-04-05 19:49:42 +0200

    Embed Google Maps.
    Co-authored-by: anubhav9454 <anubhav9454@users.noreply.github.com>

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,136 @@
 <template>
   <div>
-    
+    <div id="main-wrapper">
+      <div class="container">
+        <div id="content">
+          <!-- Content -->
+          <article>
+            <h2>About us</h2>
+            <h3>1. Company History</h3>
+            <p>TMBK @ Business was founded in 2017 with a mission to provide
+            exceptional services and support to clients navigating the
+            complexities of international travel and building maintenance. Since
+            then, we have grown into a leading provider of these services, with
+            a team of experienced professionals and a reputation for excellence.
+            Over the years, we have helped countless clients achieve their
+            goals, whether that's obtaining a visa, finding the perfect hotel,
+            or maintaining their property. We have built long-term relationships
+            with our clients, based on trust, integrity, and a deep
+            understanding of their needs. We have also expanded our services to
+            include a range of building maintenance and care solutions, helping
+            clients keep their properties in top condition. Throughout our
+            history, we have remained committed to our core values of
+            collaboration, professionalism, and respect. We believe that by
+            working together, we can achieve great things and make a positive
+            impact on the world. We are proud of our history and excited about
+            the future, as we continue to grow and expand our services to meet
+            the evolving needs of our clients.</p>
+
+            <h3>2. Mission Statement</h3>
+            <p>At TMBK @ Business, our mission is to provide exceptional services
+            and support to our clients as they navigate the complexities of
+            international travel and building maintenance. We are committed to
+            building long-term relationships with our clients, based on trust,
+            integrity, and a deep understanding of their needs. We strive to
+            deliver high-quality services, foster a culture of collaboration and
+            respect, and make a positive impact on the communities we serve. We
+            are dedicated to ongoing professional development, ensuring that our
+            team members have the skills and knowledge needed to provide expert
+            guidance and support to clients. We believe in delivering
+            personalized solutions that meet our clients' unique needs, and we
+            are committed to making the process as easy and stress-free as
+            possible. Our team members are passionate about what they do, and
+            they are driven by a desire to help clients achieve their goals.
+            Ultimately, our mission is to be the go-to provider for
+            international travel and building maintenance services, and to make
+            a positive impact on the world. We believe that by providing
+            exceptional services and fostering a culture of collaboration and
+            respect, we can achieve great things and help our clients achieve
+            their goals. At TMBK @ Business, we are committed to excellence in
+            everything we do, and we look forward to the opportunity to serve
+            you.</p>
+
+            <h3>3. Company Culture</h3>
+            <p>At TMBK @ Business, we believe that a strong company culture is the
+            foundation of our success. We are committed to fostering an
+            environment of collaboration, professionalism, and respect, where
+            every team member feels valued and supported. We believe in working
+            together to achieve our goals, whether that's helping clients
+            navigate the complexities of international travel or maintaining
+            their properties. Our team members bring a diverse range of skills
+            and expertise to the table, and we encourage open communication and
+            collaboration to ensure that every client receives the best possible
+            service. We are dedicated to ongoing professional development,
+            ensuring that our team members have the skills and knowledge needed
+            to provide expert guidance and support to clients. We are committed
+            to delivering personalized solutions that meet our clients' unique
+            needs, and we strive to build long-term relationships based on trust
+            and integrity. At the same time, we believe in having fun and
+            fostering a creative work environment. We encourage team-building
+            activities, social events, and a focus on work-life balance to
+            ensure that our team members are happy, healthy, and motivated.
+            Finally, we are committed to environmental responsibility, and we
+            strive to make a positive impact on the communities we serve. We
+            believe that by working together, we can achieve great things and
+            make a difference in the world. At TMBK @ Business, we are
+            passionate about what we do, and we look forward to helping you
+            achieve your goals.</p>
+
+            <h3>4. Team Members</h3>
+            <p>At TMBK @ Business, we are proud to have a team of experienced
+            professionals who are passionate about what they do. Our team
+            members bring a wealth of knowledge and expertise to every project,
+            and they are committed to delivering exceptional services and
+            support to our clients. We believe that our success is due in large
+            part to the dedication and hard work of our team members, and we are
+            committed to providing them with ongoing professional development
+            and opportunities for growth. Whether you are working with one of
+            our visa specialists, hotel reservation experts, or building
+            maintenance professionals, you can rest assured that you are in good
+            hands with TMBK @ Business.</p>
+
+            <h3>5. Industry Expertise</h3>
+            <p>At TMBK @ Business, we have a deep understanding of the industries
+            we serve, from travel and hospitality to building maintenance and
+            care. Our team members have years of experience working in these
+            industries, and they bring a wealth of knowledge and expertise to
+            every project. Whether you are a student looking to study abroad, a
+            business traveler in need of a hotel reservation, or a property
+            owner in need of maintenance services, we have the expertise to help
+            you achieve your goals. We stay up-to-date on the latest trends and
+            best practices in our industries, and we are committed to providing
+            our clients with the most effective and efficient solutions. When
+            you work with TMBK @ Business, you can trust that you are working
+            with a team that truly understands your needs and is dedicated to
+            helping you achieve success.</p>
+
+            <h3>6. Customer Satisfaction</h3>
+            <p>At TMBK @ Business, we are committed to providing our clients with
+            the highest level of customer satisfaction. We understand that our
+            success depends on the satisfaction of our clients, and we work
+            tirelessly to ensure that every client is happy with the services
+            and support they receive from us. From the first contact with our
+            team to the completion of the project, we strive to provide a
+            seamless and stress-free experience for our clients. We take the
+            time to listen to our clients' needs and concerns, and we work
+            closely with them to develop customized solutions that meet their
+            unique needs. Our team members are friendly, professional, and
+            responsive, and we are always available to answer any questions or
+            address any concerns our clients may have. We are proud to have a
+            reputation for excellence in customer satisfaction, and we are
+            committed to maintaining that reputation with every project we
+            undertake.</p>
+          </article>
+          <hr>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-
-  }
+export default {};
 </script>
 
 <style>
-
 </style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,25 @@
 <template>
-    <h2>This is Contact us</h2>
+  <div>
+    <div id="main-wrapper">
+      <div class="container">
+        <!-- Content -->
+        <article>
+          <h2>Contact Us</h2>
+          <p>asdasdasdas</p>
+        </article>       
+      </div>
+       <div style="height: 330px">
+          <div style="position: relative; padding-top: 0.25%; height:430px;">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2444.987636547466!2d20.977062715745724!3d52.20727216711037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471eccb120da5867%3A0xfb7ea0fa0ea255ef!2sTMBK%20Partners!5e0!3m2!1sen!2scd!4v1680716387446!5m2!1sen!2scd"
+              frameborder="0"
+              allowfullscreen
+              style="width: 100%; height: 100%"
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
+    </div>
+  </div>
 </template>
